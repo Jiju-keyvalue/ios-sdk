898bed29c26710d6240a6a28db1fd34fe9571331
Jiju S Jacob <jiju@keyvalue.systems>
2023-05-22 11:34:08 +0530

    Initial Commit

---
+++ ios_sdk/ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  ios_sdk.h
+//  ios_sdk
+//
+//  Created by JIju S Jacob on 22/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_sdk.
+FOUNDATION_EXPORT double ios_sdkVersionNumber;
+
+//! Project version string for ios_sdk.
+FOUNDATION_EXPORT const unsigned char ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_sdk/PublicHeader.h>
+
+
